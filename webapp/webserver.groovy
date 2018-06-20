790ff6327b736d2180c64fa3b1da0bd41ce12b77
neodevelop <juan@makingdevs.com>
2018-06-20 12:14:03 -0500

    Primer servidor web con una respuesta simple

--- webapp/webserver.groovy
+++ webapp/webserver.groovy
@@ -1,1 +1,10 @@
-println vertx.createHttpServer()
+httpServer = vertx.createHttpServer()
+
+httpServer.requestHandler { request ->
+  request
+  .response()
+  .putHeader("content-type", "text/plain")
+  .end("Hello world")
+}
+
+httpServer.listen(1234)
