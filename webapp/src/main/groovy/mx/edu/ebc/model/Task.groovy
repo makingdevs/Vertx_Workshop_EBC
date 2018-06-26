ba72878fded6747b4f319ff412107d483f8caf09
neodevelop <juan@makingdevs.com>
2018-06-26 13:22:43 -0500

    Haciendo el insert en la base

---
+++ webapp/src/main/groovy/mx/edu/ebc/model/Task.groovy
@@ -0,0 +1,8 @@
+package mx.edu.ebc.model
+
+class Task {
+  Integer id
+  String description
+  String status
+  Date date_created
+}
--- webapp/src/main/groovy/mx/edu/ebc/verticles/data_access_verticle.groovy
+++ webapp/src/main/groovy/mx/edu/ebc/verticles/data_access_verticle.groovy
@@ -1,6 +1,8 @@
 package mx.edu.ebc.verticles
 
+import mx.edu.ebc.model.Task
 import io.vertx.ext.asyncsql.MySQLClient
+import io.vertx.core.json.JsonObject
 
 def mySQLClientConfig = [
   host: "localhost",
@@ -29,4 +31,21 @@ vertx.eventBus().consumer("mx.edu.ebc.data.find_one_task"){ message ->
       println dbResult.result()
     })
   }
+}
+
+vertx.eventBus().consumer("mx.edu.ebc.data.save_task"){ message ->
+  mySQLClient.getConnection { result ->
+    def connection = result.result()
+    def json = new JsonObject(message.body())
+    def params = [json.map.description, json.map.status]
+    connection.updateWithParams("INSERT INTO task(description, status, date_created) values(?,?,now())", params, { dbResult ->
+      println dbResult.result()
+    })
+  }
+}
+
+
+vertx.eventBus().consumer("mx.edu.ebc.data.send_to_save"){ message ->
+  Task task = new Task(description: "${new Date()}", status: "TODO")
+  vertx.eventBus().send("mx.edu.ebc.data.save_task", JsonObject.mapFrom(task))
 }
