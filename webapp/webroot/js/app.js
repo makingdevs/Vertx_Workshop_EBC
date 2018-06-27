12acb152b36f2324716928ae4f9290faba4a6e97
neodevelop <juan@makingdevs.com>
2018-06-27 12:49:39 -0500

    Agrego app.js e inicio el EventBus con un consumer

--- webapp/webroot/js/app.js
+++ webapp/webroot/js/app.js
@@ -1,2 +1,9 @@
-
-
+var eb = new EventBus("http://localhost:1234/eventbus")
+eb.enableReconnect(true);
+eb.onopen = function(){
+  console.log("Connecting to Eventbus...");
+  eb.registerHandler("mx.edu.ebc.clock", function(error, message){
+    console.log(message);
+  });
+  console.log("Ready 😎 ...");
+};
