0b3b51c57f067918d5a73addf8dbfc96afe031e2
neodevelop <juan@makingdevs.com>
2018-06-28 18:33:42 -0500

    Agrego procesamiento masivo de archivos

--- webapp/src/main/groovy/mx/edu/ebc/verticles/consumer_massive_verticle.groovy
+++ webapp/src/main/groovy/mx/edu/ebc/verticles/consumer_massive_verticle.groovy
@@ -1,8 +1,16 @@
 package mx.edu.ebc.verticles
 
+import io.vertx.core.json.JsonObject
+
+def sharedData = vertx.sharedData()
+def trips = sharedData.getLocalMap("trips")
+
 println "*** Ready for listen ${this.class.name} 😎 ***"
 vertx.eventBus().consumer("mx.edu.ebc.do_operation"){ msg ->
-  Thread.sleep(100)
-  println msg.body().toInteger()
+  def tripString = msg.body()
+  def trip = tripString.split(',')
+  def processed = trips.processed.add(tripString)
+  trips.put("processed", processed)
+  vertx.eventBus().send("mx.edu.ebc.massive_processing_done", trip.hashCode())
 }
 
--- webapp/src/main/groovy/mx/edu/ebc/verticles/producer_massive_verticle.groovy
+++ webapp/src/main/groovy/mx/edu/ebc/verticles/producer_massive_verticle.groovy
@@ -1,8 +1,28 @@
 package mx.edu.ebc.verticles
 
+def sharedData = vertx.sharedData()
+def trips = sharedData.getLocalMap("trips")
+
+trips.put("processed", [])
+
 vertx.eventBus().consumer("mx.edu.ebc.massive_processing"){ msg ->
-  println "#"*100
-  (1..1000).each { n ->
-    vertx.eventBus().send("mx.edu.ebc.do_operation", n)
+  vertx.fileSystem().readFile("src/main/resources/2010-02.csv", { result ->
+    if (result.succeeded()) {
+      def lines = result.result().toString().split("\n")
+      trips.size = lines.size()
+      lines.each { l ->
+        vertx.eventBus().send("mx.edu.ebc.do_operation", l)
+      }
+    } else {
+      System.err.println("Oh oh ...${result.cause()}")
+    }
+  })
+}
+
+vertx.eventBus().consumer("mx.edu.ebc.massive_processing_done"){ msg ->
+  println msg.body()
+  println "${trips.size} == ${trips.processed.size()}"
+  if(trips.size == trips.processed.size()){
+    println "*** Done 👊 ***"
   }
 }
