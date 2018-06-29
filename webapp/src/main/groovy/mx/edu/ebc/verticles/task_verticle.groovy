3e199aa8de303df333814f7b39b755af14525876
neodevelop <juan@makingdevs.com>
2018-06-29 09:27:42 -0500

    Uso JsonObject para responder la lista de Task con un par de ellas...

--- webapp/src/main/groovy/mx/edu/ebc/verticles/task_verticle.groovy
+++ webapp/src/main/groovy/mx/edu/ebc/verticles/task_verticle.groovy
@@ -1,9 +1,13 @@
 package mx.edu.ebc.verticles
 
+import mx.edu.ebc.model.Task
+import io.vertx.core.json.JsonObject
+
 vertx.eventBus().consumer("mx.edu.ebc.task.all"){ msg ->
   println "*"*100
   println msg
   println "*"*100
-  msg.reply("Hellpo back")
+  def response = [new Task(), new Task()].collect { t -> JsonObject.mapFrom(t) }
+  msg.reply(response)
 }
 
